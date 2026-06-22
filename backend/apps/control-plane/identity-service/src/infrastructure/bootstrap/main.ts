c7610236f5e0b5ce26ffb18cac3e90f3c7295f96
iSenity1812 <pvduchuya4@gmail.com>
2026-06-22 12:16:40 +0700

    feat(identity-svc): add cors config

--- backend/apps/control-plane/identity-service/src/infrastructure/bootstrap/main.ts
+++ backend/apps/control-plane/identity-service/src/infrastructure/bootstrap/main.ts
@@ -11,6 +11,11 @@ async function bootstrap(): Promise<void> {
   let globalPrefix = process.env.API_PREFIX || "api/v1";
   app.setGlobalPrefix(globalPrefix);
 
+  app.enableCors({
+    origin: process.env.CORS_ORIGIN || "http://localhost:3000",
+    credentials: true,
+  });
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
